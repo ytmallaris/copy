89de8cf79ec66baf01b50241873b5d0156485cae
ytmallaris <72230437+ytmallaris@users.noreply.github.com>
2020-10-16 18:19:13 -0400

    Create copy.js

---
+++ copy.js
@@ -0,0 +1,8 @@
+function copy(text) {
+  var textarea = document.createElement("textarea");
+  textarea.value = text;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textarea);
+}
